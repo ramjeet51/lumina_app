0ccac31e22ade4f9e65f5bff5752f71cc1dc4277
Ubuntu <ubuntu@ip-172-31-28-139.ec2.internal>
2026-06-13 08:16:49 +0000

    change for docker

--- backend/main.py
+++ backend/main.py
@@ -20,8 +20,8 @@ app = FastAPI(title="Auth API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
